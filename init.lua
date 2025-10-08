8e887b4b3d0442d6cde9abb568970d4e7ab6676e
Dave <david@fruincom.com>
2025-10-08 14:41:12 -0700

    Telescope now searches in the current directory with <leader>fc

--- init.lua
+++ init.lua
@@ -268,14 +268,18 @@ end)
 
 pcall(function()
 	local builtin = require("telescope.builtin")
-	vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: Find files" })
 	vim.keymap.set("n", "<leader>ff", function()
 		builtin.find_files({ hidden = true })
 	end, { desc = "Telescope: Find files" })
 	vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope: Live grep" })
 	vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope: Switch buffers" })
 	vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope: Help tags" })
-	vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Telescope: Commands" })
+	vim.keymap.set("n", "<leader>fc", function()
+		builtin.find_files({
+			cwd = vim.fn.expand("%:p:h"),
+			hidden = true,
+		})
+	end, { desc = "Telescope: Find files in current buffer dir" })
 	vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Telescope: Keymaps" })
 end)
 
